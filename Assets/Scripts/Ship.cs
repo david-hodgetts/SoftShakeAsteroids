b16d6e4f4325eaa11f967a869047c85aff0129d3
david hodgetts <david.demainlalune@gmail.com>
2013-10-22 16:29:27 +0200

    add rotation to ship

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+	
+	public float rotationSpeed = 300;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+		transform.Rotate(0, rotation, 0);
+	}
+}
