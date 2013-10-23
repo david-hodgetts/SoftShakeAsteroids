d5ebbf44d80c20c96654a536da48ac90bc758e18
david hodgetts <david.demainlalune@gmail.com>
2013-10-23 10:46:37 +0200

    asteroid movement

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent (typeof (Rigidbody))]
+public class Asteroid : MonoBehaviour {
+
+	public float startForce = 100;
+
+
+	void Start () {
+		Vector2 randomDirection = Random.insideUnitCircle.normalized * startForce;
+		Vector3 direction = new Vector3(randomDirection.x, 0, randomDirection.y);
+	
+		rigidbody.AddForce(direction);
+	}
+}
