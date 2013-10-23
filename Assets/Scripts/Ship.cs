339751e3e23429520ea701be3b8b5ac829732b2b
david hodgetts <david.demainlalune@gmail.com>
2013-10-23 11:02:09 +0200

    implement Ship#Shoot

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,12 +7,17 @@ public class Ship : MonoBehaviour {
 	public float rotationSpeed = 300;
 	public float thrustForce = 20;
 
-
+	public Rigidbody bulletPrefab;
+	public float bulletForce = 1000;
 	
 	// Update is called once per frame
 	void Update () {
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 		transform.Rotate(0, rotation, 0);
+		
+		if(Input.GetMouseButtonDown(1))
+			Shoot();
+		
 	}
 	
 	// called every fixed framerate frame (physics)
@@ -22,4 +27,15 @@ public class Ship : MonoBehaviour {
 			rigidbody.AddForce(force);
 		}
 	}
+	
+	void Shoot(){
+		Rigidbody bullet = Instantiate(bulletPrefab, 
+									   transform.position + transform.forward, 
+									   Quaternion.identity) as Rigidbody;
+		
+		bullet.rigidbody.AddForce(transform.forward * bulletForce);
+		
+	}
+	
+	
 }
